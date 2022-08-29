0cff1674e274e8040d169a5b0a221dc242cedabe
yeomboyeon <103543320+yeomboyeon@users.noreply.github.com>
2022-08-29 14:26:32 +0900

    JDBC) article list 시 데이터를 DB로 부터 가져오기
    테스트 성공한 코드를 바탕으로 main 으로 옮기기

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 package com.KoreaIT.JP;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,8 +14,6 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("==프로그램 시작==");
-
-		List<Article> articles = new ArrayList<>();
 
 		int lastArticleId = 0;
 
@@ -37,14 +40,116 @@ public class Main {
 				System.out.println("body : " + body);
 
 				Article article = new Article(id, title, body);
-				articles.add(article);
+
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JDBCTest?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "INSERT INTO article";
+					sql += " SET regDate = NOW()";
+					sql += ", updateDate = NOW()";
+					sql += ", title = '" + title + "'";
+					sql += ", `body` = '" + body + "';";
+
+					System.out.println(sql);
+					pstmt = conn.prepareStatement(sql);
+
+					int affectedRows = pstmt.executeUpdate();
+
+					System.out.println(affectedRows + "열에 적용됨");
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+
 				lastArticleId++;
 
-				System.out.println(article);
-
-				continue;
 			} else if (cmd.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
+
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+
+				List<Article> articles = new ArrayList<>();
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JDBCTest?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "SELECT *";
+					sql += " FROM article";
+					sql += " ORDER BY id DESC";
+
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+					rs = pstmt.executeQuery();
+
+					while (rs.next()) {
+						int id = rs.getInt("id");
+						String regDate = rs.getString("regDate");
+						String updateDate = rs.getString("updateDate");
+						String title = rs.getString("title");
+						String body = rs.getString("body");
+
+						Article article = new Article(id, regDate, updateDate, title, body);
+						articles.add(article);
+					}
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (rs != null && !rs.isClosed()) {
+							rs.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 
 				if (articles.size() == 0) {
 					System.out.println("게시물이 없습니다");
@@ -53,7 +158,7 @@ public class Main {
 
 				System.out.println("번호    |    제목");
 
-				for (int i = articles.size() - 1; i >= 0; i--) {
+				for (int i = 0; i < articles.size(); i++) {
 					Article article = articles.get(i);
 					System.out.printf("%4d    |    %s\n", article.id, article.title);
 				}
